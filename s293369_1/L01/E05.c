b84356cea75dfbe9b78a12dcb45175df95506363
SimoneMoreWare <59175940+SimoneMoreWare@users.noreply.github.com>
2022-03-06 21:52:18 +0100

    Create E05.c

---
+++ s293369_1/L01/E05.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int main() {
+    char operation;
+    float op1,op2;
+    float result;
+
+    printf("inserire operazione da eseguire ");
+    operation = getchar();
+
+    printf("inserisci valore 1 ");
+    scanf("%f",&op1);
+    printf("inserisci valore 2 ");
+    scanf("%f",&op2);
+
+    switch(operation){
+        case '+':
+            result = op1 + op2;
+            break;
+        case '-':
+            result = op1-op2;
+            break;
+        case '/':
+            if (op2!=0){
+                result=op1/op2;
+            } else{
+                printf("impossibile eseguire operazione");
+            }
+            break;
+        case '*':
+            result = op1*op2;
+            break;
+        default:
+            printf("hai digitato male");
+    }
+    printf("%f",result);
+    return 0;
+}
