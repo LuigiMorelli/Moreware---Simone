1d97c74fde1078bc94cabb5090f48f74e6dd9e1f
SimoneMoreWare <59175940+SimoneMoreWare@users.noreply.github.com>
2022-03-31 21:47:31 +0200

    Update E03.c

--- s293369_1/L02/EO3/E03.c
+++ s293369_1/L02/EO3/E03.c
@@ -39,7 +39,7 @@ int main(){
     FILE *fp_input,*fp_write;
     char scelta;
 
-    fp_input = fopen("../compresso.txt","r");
+    fp_input = fopen("../input0.txt","r");
     fp_write = fopen("../decompresso.txt","w");
 
     printf("Decodificare (D) o Codificare (C) ");
@@ -60,27 +60,37 @@ int main(){
 
 void comprimi(FILE *fp_input, FILE *fp_write){
     char c1,c2,c3;
-    int count =0;
+    int count =1;
     int endword;
 
     fscanf(fp_input,"%c%c",&c1,&c2);
-    fprintf(fp_write,"%c",c1);
+    if(c1!=c2){
+        fprintf(fp_write,"%c",c1);
+    }
+
     while(!feof(fp_input) || c3==EOF) {
         fscanf(fp_input,"%c",&c3);
         if (c2 == c1) {
-            count = count + 1;
+
             endword = (c2!=c3 || c3==EOF);
+
+            if(count==1 && c2!=c3){
+                fprintf(fp_write,"%c%c",c2,c2);
+            }
             if (count >= 2 && count <= 9 && endword) {
-                fprintf(fp_write,"%c$%d", c2, count);
+                    fprintf(fp_write,"%c$%d", c2, count);
             } else if (count > 9) {
                 fprintf(fp_write,"%c$%d", c2, count-1);
-                count = 0;
+                count = 1;
             }
+            count = count + 1;
+
         }else{
             if (c2!=c3 || c3==EOF){
                 fprintf(fp_write,"%c",c2);
             }
-            count=0;
+
+            count=1;
 
         }
         c1 = c2;
@@ -89,9 +99,9 @@ void comprimi(FILE *fp_input, FILE *fp_write){
 }
 
 void decomprimi(FILE *fp_input, FILE *fp_write){
-   char c1,c2,c3;
-   int count;
-   int i;
+    char c1,c2,c3;
+    int count;
+    int i;
     fscanf(fp_input,"%c%c",&c1,&c2);
     fprintf(fp_write,"%c",c1);
     while(!feof(fp_input)) {
